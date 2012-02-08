04dd4ebbd92b3113efc7d256b661e660480b62c7
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2012-02-08 18:51:13 +0530

    A basic dialog box is added

--- lyrics-downloader.py
+++ lyrics-downloader.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 from gi.repository import GObject, Peas, GdkPixbuf, Gtk, Notify # pylint: disable-msg=E0611
-from gi.repository import Totem # pylint: disable-msg=E0611
+from gi.repository import Totem, Gio # pylint: disable-msg=E0611
 
 class LyricsPlugin (GObject.Object, Peas.Activatable):
     __gtype_name__ = 'LyricsPlugin'
@@ -11,13 +11,17 @@ class LyricsPlugin (GObject.Object, Peas.Activatable):
     def __init__ (self):
         GObject.Object.__init__ (self)
 
+        self._dialog = None
         self._totem = None
-
+        
         self._manager = None
         self._menu_id = None
         self._action_group = None
         self._action = None
 
+        self._close_button = None
+        self._info_label = None
+        
     # totem.Plugin methods
 
     def do_activate (self):
@@ -31,8 +35,13 @@ class LyricsPlugin (GObject.Object, Peas.Activatable):
         self._manager = self._totem.get_ui_manager ()
         self._append_menu()
 
+        self._totem.connect ('file-opened', self.__on_totem__file_opened)
+        self._totem.connect ('file-closed', self.__on_totem__file_closed)
+
     def do_deactivate (self):
         # Include the Plugin destroying Actions
+        if self._dialog:
+            self._dialog.destroy()
         self._totem = None
 
         self._delete_menu()
@@ -64,21 +73,58 @@ class LyricsPlugin (GObject.Object, Peas.Activatable):
         self._manager.ensure_update ()
 
         self._action.connect ('activate', self._show_dialog)
-        """
-        
+
         self._action.set_sensitive (self._totem.is_playing () and
-        self._check_allowed_scheme () and
-        not self._check_is_audio ())
+                                    self._check_is_mp3())
+
+    def _check_is_mp3 (self):
         """
+        This check if the file playing is mp3
+
+        """
+        filename = self._totem.get_current_mrl ()
+        if Gio.content_type_guess (filename, '')[0] == 'audio/mpeg':
+            return True
+        return False
+
+    def _close_dialog(self, ):
+        """
+        Dialog box is hidden instead of destroying it
+        """
+        self._dialog.hide()
+        
 
     def _delete_menu (self):
         self._manager.remove_action_group (self._action_group)
         self._manager.remove_ui (self._menu_id)
+
+    def _build_dialog(self, ):
+        """
+        Builds the main gui of the lyrics plugin
+        
+        """
+        builder = Totem.plugin_load_interface ("lyrics-downloader",
+                                               "lyrics-downloader.ui", True,
+                                               self._totem.get_main_window (),
+                                               self)
+        self._dialog = builder.get_object ('lyrics_dialog')
+        self._close_button = builder.get_object ('close_button')
+        self._info_label = builder.get_object ('info_label')
+        
+        # Set up signals
+        self._close_button.connect ('clicked', self.__on_close_clicked)
+        
+        self._dialog.connect ('delete-event', self._dialog.hide_on_delete)
+        self._dialog.set_transient_for (self._totem.get_main_window ())
         
     def _show_dialog (self, _action):
-        self._show_notification("Download Lyrics Clicked",
-                                "Method still not implemented")
+        if not self._dialog:
+            self._build_dialog()
 
+        self._info_label.set_text("This is manually set")
+
+        self._dialog.show_all()
+        
     def _show_notification (self,title,description):
         Notify.init("Totem Lyrics Plugin")
         
@@ -93,10 +139,41 @@ class LyricsPlugin (GObject.Object, Peas.Activatable):
     def _get_file_path(self, filename):
         """
         This method returns the a absolute path for the file.
-        This should be used by plugins to find plugin-specific resource files.
+        This should be used by plugin to find plugin-specific resource files.
     
         Arguments:
         - `filename`: filename of the resource file
         """
         return Totem.plugin_find_file("lyrics-downloader",filename)
+
+    def __on_close_clicked(self, _data):
+        """
+        Clicke event of close button in the dialog box
+    
+        Arguments:
+        - `_data`: 
+        """
+        self._close_dialog()
         
+    def __on_totem__file_opened(self, _totem, new_mrl):
+        """
+        This method will be called when a file is opened
+        
+        Arguments:
+        - `_totem`: A TotemObject
+        - `new_mrl`: the MRL opened
+        """
+        if self._check_is_mp3():
+            self._action.set_sensitive(True)
+        else:
+            self._action.set_sensitive(False)
+
+    def __on_totem__file_closed(self, _totem):
+        """
+        This method is called when the file is closed
+    
+        Arguments:
+        - `_totem`: A TotemObject
+        """
+
+        self._action.set_sensitive(False)
